c2a05d5e40e61003f1f91c99ea7cd6353a276fdc
ALI AKBAR <captian.8089@gmail.com>
2022-12-07 17:26:46 +0530

    add env file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-
+require("dotenv").config()
 app.get('/', (req, res) => {
     res.send("hello world it's me ALI AKBAR")
 })
 
-app.listen(3001,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("server start at port 3001");
 })
